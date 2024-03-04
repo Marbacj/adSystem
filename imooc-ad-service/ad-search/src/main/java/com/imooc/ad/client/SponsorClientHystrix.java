e9276dcf5dd2bd4d765835182c4633322285f2ac
Marbacj <Uncle.YuH@outlook.com>
2024-03-04 19:40:53 +0800

    1

---
+++ imooc-ad-service/ad-search/src/main/java/com/imooc/ad/client/SponsorClientHystrix.java
@@ -0,0 +1,23 @@
+package com.imooc.ad.client;
+
+import com.imooc.ad.client.vo.AdPlan;
+import com.imooc.ad.client.vo.AdPlanGetRequest;
+import com.imooc.ad.vo.CommonResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+//引入断路器是为了防止雪崩
+//实现服务降级
+/**
+ * Created by Marbach
+ */
+@Component
+public class SponsorClientHystrix implements SponsorClient {
+
+    @Override
+    public CommonResponse<List<AdPlan>> getAdPlans(
+            AdPlanGetRequest request) {
+        return new CommonResponse<>(-1,
+                "eureka-client-ad-sponsor error");
+    }
+}
